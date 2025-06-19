4615c96afdd1a9bad786da18a1f8b8b256ccfc52
TomekWojdyla <tw.kota@gmail.com>
2025-06-19 21:52:58 +0200

    REST invoice->product. Getting product names for invoice

---
+++ InvoiceService.Application/Infrastructure/Services/ProductServiceClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using InvoiceService.Domain.Models;
+
+namespace InvoiceService.Application.Infrastructure.Services;
+
+public class ProductServiceClient : IProductServiceClient
+{
+    private readonly HttpClient _httpClient;
+
+    public ProductServiceClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<string> GetProductNameByIdAsync(int productId)
+    {
+        var response = await _httpClient.GetAsync($"/api/Books/{productId}");
+
+        if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            return "Book does not exist.";
+
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+        var product = JsonSerializer.Deserialize<Book>(content, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        return product?.Name ?? "Unknown";
+    }
+}
